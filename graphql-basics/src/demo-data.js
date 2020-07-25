e0b937db96a8fcdb30c3d91cdf29120b12c732b6
devinhenkel <devinhenkel@gmail.com>
2020-07-25 00:25:09 -0500

    added mutations

---
+++ graphql-basics/src/demo-data.js
@@ -0,0 +1,116 @@
+// demo data
+var users = [
+    {
+        id: '1',
+        username: 'Devin Henkel',
+        firstname: 'Devin',
+        lastname: 'Henkel-Legare',
+        email: 'devin@devinhenkel.com'
+    },
+    {
+        id: '2',
+        username: 'lauralegare',
+        firstname: 'Laura',
+        lastname: 'Legare',
+        email: 'lauralegare@live.com'
+    },
+    {
+        id: '3',
+        username: 'hopelesscalico',
+        firstname: 'Calliope',
+        lastname: 'Legare',
+        email: 'callie.legare@icloud.com'
+    },
+    {
+        id: '4',
+        username: 'brozuni',
+        firstname: 'Edison',
+        lastname: 'Legare',
+        email: 'edison.legare@icloud.com'
+    },
+    {
+        id: '5',
+        username: 'mcbattlepanda',
+        firstname: 'Emmett',
+        lastname: 'Legare',
+        email: 'emmett.legare@icloud.com'
+    }
+]
+
+var projects = [
+    {
+        id: 'aaa',
+        name: 'Project 1',
+        description: 'A starter project',
+        active: true,
+        owner: '1'
+    },
+    {
+        id: 'bbb',
+        name: 'Project 2',
+        description: 'Another starter project',
+        active: false,
+        owner: '3'
+    },
+    {
+        id: 'ccc',
+        name: 'Manhattan Project',
+        description: 'I am become death',
+        active: false,
+        owner: '1'
+    },
+    {
+        id: 'ddd',
+        name: 'Project Runway',
+        description: 'I am become fierce',
+        active: true,
+        owner: '5'
+    }
+]
+
+
+
+var risks = [
+    {
+        id: '100',
+        title: 'Risk 1',
+        description: 'Something bad',
+        author: '2',
+        project: 'bbb',
+        branch: null
+    },
+    {
+        id: '200',
+        title: 'Risk 2',
+        description: 'My spider sense is tingling',
+        author: '4',
+        project: 'aaa',
+        branch: '100'
+    },
+    {
+        id: '300',
+        title: 'Risk 3',
+        description: 'I got a bad feeling about this',
+        author: '1',
+        project: 'ccc',
+        branch: null
+    },
+    {
+        id: '400',
+        title: 'Risk 4',
+        description: 'Duck',
+        author: '5',
+        project: 'ddd',
+        branch: '300'
+    },
+    {
+        id: '500',
+        title: 'Risk 5',
+        description: 'Look out!',
+        author: '5',
+        project: 'ddd',
+        branch: '300'
+    }
+]
+
+export { users, projects, risks }
---
+++ graphql-basics/src/index-sample-resolvers.js
@@ -0,0 +1,117 @@
+import { GraphQLServer } from 'graphql-yoga';
+const axios = require('axios');
+
+// type definitions
+const typeDefs = `
+    type Query {
+        add(numbers: [Float!]!): Float!
+        starwars(id: Int): Character!
+        me: User!
+        grades(index: Int): [Int!]!
+        project: Project!
+        ronswanson: String!
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        firstname: String
+        lastname: String
+        email: String!
+        age: Int
+    }
+
+    type Project {
+        id: ID!
+        name: String!
+        description: String
+    }
+
+    type Character {
+        name: String!
+        height: String
+        gender: String
+    }
+`
+
+// resolvers
+const resolvers = {
+    Query: {
+        add(parent, {numbers}, context, info) {
+            return numbers.reduce((accumulator, currentValue) => {
+                return accumulator + currentValue
+            })
+        },
+        me() {
+            return {
+                id: `123`,
+                username: 'devinhenkel',
+                firstname: 'Devin',
+                email: 'devinhenkel@gmail.com'
+            }
+        },
+        project() {
+            return {
+                id: `999`,
+                name: 'My First Project',
+                description: 'My initial project instance'
+            }
+        },
+        grades(parent, args, context, info){
+            var grades = [10,33,12,76,84,99]
+            if (args.index !== null && args.index > -1 && args.index < grades.length) {
+                return [grades[args.index]]
+            }
+            return grades
+        },
+        async ronswanson() {
+            const rsurl = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
+            var quote = ''
+            await axios.get(rsurl)
+            .then(function(res){
+                console.log(res.data[0])
+                quote = res.data[0]
+            })
+            .catch(function(err){
+                quote = 'Error!'
+                if (err.response){
+                console.log("Problem with response", err.response.status);
+                } else if (err.request) {
+                console.log("Problem with request");
+                } else {
+                console.log("Error: ", err.message);
+                }
+            })
+            return quote
+        },
+        async starwars(parent, {id}) {
+            const rsurl = `https://swapi.dev/api/people/${id}/`
+            var character = ''
+            await axios.get(rsurl)
+            .then(function(res){
+                console.log(res.data)
+                character = { name: res.data['name'], height: res.data['height'], gender: res.data['gender'] }
+            })
+            .catch(function(err){
+                character = 'Error!'
+                if (err.response){
+                console.log("Problem with response", err.response.status);
+                } else if (err.request) {
+                console.log("Problem with request");
+                } else {
+                console.log("Error: ", err.message);
+                }
+            })
+            return character
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('Server is running.')
+})
